5edeae6a06682ea26ba350da5e0ea776a1a8b20d
emestiza <emestiza@u.rochester.edu>
2020-09-12 21:02:38 -0400

    Heroku issue

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+
 import environ
 
 env = environ.Env()
 # reading .env file
 environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
